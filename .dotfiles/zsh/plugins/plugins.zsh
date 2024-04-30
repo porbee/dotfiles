d64c639488dde8fe8e5111476add428ed83a607e
porbee <148717933+porbee@users.noreply.github.com>
2024-04-30 22:17:26 +0200

    Add files via upload

---
+++ .dotfiles/zsh/plugins/plugins.zsh
@@ -0,0 +1,7 @@
+# Fast syntax highlighting
+[ -f ~/.config/zsh/plugins/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh ] && source ~/.config/zsh/plugins/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
+
+# Syntax highlighting
+
+# Zsh autosuggestions
+[ -f ~/.config/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh ] && source ~/.config/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
